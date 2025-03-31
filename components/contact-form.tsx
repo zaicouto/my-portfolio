cd391cf4f66c177fe3dad87ab01a31c5d38ba891
Marlon Couto <marloncouto719@gmail.com>
2025-03-31 13:18:35 -0300

    feat: update contact form to use FormData and integrate with Netlify

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -17,36 +17,45 @@ export default function ContactForm() {
     e.preventDefault();
     setIsSubmitting(true);
 
-    const formData = {
-      email: e.currentTarget.email.value,
-      name: e.currentTarget.firstName.value,
-      message: e.currentTarget.message.value,
-    };
-    
-    if (!formData.email || !formData.name || !formData.message) {
-      toast("Por favor, preencha todos os campos.");
-      setIsSubmitting(false);
-      return;
-    }
-    
-    console.log('formData :>> ', JSON.stringify(formData));
-    
+    // const formData = {
+    //   email: e.currentTarget.email.value,
+    //   name: e.currentTarget.firstName.value,
+    //   message: e.currentTarget.message.value,
+    // };
+
+    // if (!formData.email || !formData.name || !formData.message) {
+    //   toast("Por favor, preencha todos os campos.");
+    //   setIsSubmitting(false);
+    //   return;
+    // }
+
+    // console.log('formData :>> ', JSON.stringify(formData));
+
     try {
-      const response = await axios.post("/api/send", formData, {
+      // const response = await axios.post("/api/send", formData, {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // });
+
+      // if (response.data.error) {
+      //   console.error(response.data.error);
+      //   toast("Erro ao enviar mensagem. Tente novamente mais tarde.");
+      //   setIsSubmitting(false);
+      //   return;
+      // }
+
+      const formData = new FormData(e.currentTarget);
+
+      await axios.post("__contact-form.html", {
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
         },
+        body: new URLSearchParams(formData as any).toString(),
       });
-      
-      if (response.data.error) {
-        console.error(response.data.error);
-        toast("Erro ao enviar mensagem. Tente novamente mais tarde.");
-        setIsSubmitting(false);
-        return;
-      }
-      
+
       toast("Mensagem enviada! Obrigado por entrar em contato.");
-      
+
       setIsSubmitting(false);
       e.currentTarget.reset();
     } catch (error) {
@@ -58,14 +67,26 @@ export default function ContactForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4" name="contact">
+      <input type="hidden" name="form-name" value="contact" />
       <div className="space-y-2">
         <Label htmlFor="firstName">Nome</Label>
-        <Input id="firstName" placeholder="Seu nome" required />
+        <Input
+          id="firstName"
+          placeholder="Seu primeiro nome"
+          required
+          name="firstName"
+        />
       </div>
       <div className="space-y-2">
         <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" placeholder="Seu email" required />
+        <Input
+          id="email"
+          type="email"
+          placeholder="Seu email"
+          required
+          name="email"
+        />
       </div>
       <div className="space-y-2">
         <Label htmlFor="message">Mensagem</Label>
@@ -74,6 +95,7 @@ export default function ContactForm() {
           placeholder="Sua mensagem"
           className="min-h-[120px]"
           required
+          name="message"
         />
       </div>
       <Button type="submit" className="w-full" disabled={isSubmitting}>
