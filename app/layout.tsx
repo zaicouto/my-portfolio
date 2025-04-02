cd6ea6d05277d24e5109be7feb5dfbb8b90dc80c
Marlon Couto <marloncouto719@gmail.com>
2025-04-02 19:01:31 -0300

    fix: restructure layout to include Navbar and Footer components in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import FooterComponent from "@/components/footer-component";
+import NavbarComponent from "@/components/navbar-component";
 
 // TODO: adicionar mais metadados
 export const metadata: Metadata = {
@@ -14,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className="min-h-screen bg-background">
+        <NavbarComponent />
+        <main className="container py-10 mx-auto">{children}</main>
+        <Toaster />
+        <FooterComponent />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
-import NavbarComponent from "@/components/navbar-component";
 import HeroSection from "@/components/sections/hero-section";
 import AboutSection from "@/components/sections/about-section";
 import SkillsSection from "@/components/sections/skills-section";
 import ProjectsSection from "@/components/sections/projects-section";
-import FooterComponent from "@/components/footer-component";
 import ContactSection from "@/components/sections/contact-section";
 
 // TODO: usar gsap para animar as seções
 
 export default function Portfolio() {
   return (
-    <div className="min-h-screen bg-background">
-      <NavbarComponent />
-
-      <main className="container py-10 mx-auto">
-        <HeroSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <ContactSection />
-      </main>
-
-      <FooterComponent />
-    </div>
+    <>
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ContactSection />
+    </>
   );
 }
