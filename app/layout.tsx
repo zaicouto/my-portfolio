7efa0566658638b625ff6474eaacea9ded6401c0
Marlon Couto <marloncouto719@gmail.com>
2025-04-03 15:30:40 -0300

    fix: integrate Inter font into layout for improved typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import FooterComponent from "@/components/footer-component";
 import NavbarComponent from "@/components/navbar-component";
+import { Inter } from "next/font/google";
 
 // TODO: adicionar mais metadados
 export const metadata: Metadata = {
@@ -10,13 +11,18 @@ export const metadata: Metadata = {
   description: "Portfolio online de Marlon Couto, desenvolvedor full-stack",
 };
 
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={inter.className}>
       <body className="min-h-screen bg-background">
         <NavbarComponent />
         <main className="container py-10 mx-auto">{children}</main>
